28ed86a016f65d0b13a6b8b2c128a1c33ae22328
songen <614750803@qq.com>
2023-02-06 23:59:42 +0800

    添加viper管理config配置

--- main.go
+++ main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"aaa/common"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
+	InitConfig()
 	db := common.InitDB()
 	println(db, "22")
 
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
 	panic(r.Run()) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("")
+	}
 }
